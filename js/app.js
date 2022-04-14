c903508d5b412d5b8bda5ec1b8e3a345d05c7f98
xchanin <shannonwbruns@gmail.com>
2022-04-14 14:27:19 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log(JSON.parse('{"name":"John", "age":30, "city":"New York"}'));
+const json = [
+    {
+        "id": 3001,
+        "name": "Dom"
+    },
+    {
+        "id": 3098,
+        "name": "Bob"
+    },
+    {
+        "id": 3021,
+        "name": "Shannon"
+    }
+]
+
+const stringyJSON = JSON.stringify(json);
+console.log(stringyJSON);
