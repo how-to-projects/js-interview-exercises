4033d8d339c50196447439d12cb7bbf7b814817a
xchanin <shannonwbruns@gmail.com>
2022-04-15 10:51:02 -0600

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,48 +1,21 @@
 /**
- * Palindrome - a word, phrase, or sequence that reads
- * the same forward or backward. e.g., madam, mom, nurses run
+ * Taxes - write a function that take two arguments, an amout in dollars
+ * and a tax percentage. Return an array with the tax amount in cents.
+ * 
+ * Return an array? Whatever, turd.
+ * 
+ * This is a good example of questioning the interviewer; why
+ * they want you to return the value in an array, it doesn't make
+ * sense. This is really the reason for the question, not to 
+ * necessarily be able to solve it. 
  */
 
-/**
- * Test if the value given is a palindrome - probably many ways to do this,
- * but I think this is one of the simpliest - could use RegEx or somethihg like that
- * 
- * This method could be simplified, but expanding it for clarity 
- * 
- * @param val - value to test for palindrome 
- * @returns boolean if the given value is the same forward and back
- */
-const isPalindrome = (val) => {
+const taxAmountInCents = (amount, percent) => {
 
-    /**
-     * Remove any whitespace
-     */
-    const removeWhiteSpace = val.replace(' ', '');
-    
-    /**
-     * create an array from string charactes
-     */
-    const splitStringToArray = removeWhiteSpace.split('');
+    const taxAmount = (amount * percent).toFixed(2);
+    const taxAmountArr = [taxAmount];
 
-    /**
-     * Reverse the array
-     */
-    const reverseArray = splitStringToArray.reverse();
-
-    /**
-     * Create a string from the reverse array
-     */
-    const reversedString = reverseArray.join('');
-
-    /**
-     * Check if the original value, minuse the white space is
-     * equal to the newly created reversed string
-     */
-    if (reversedString === removeWhiteSpace) {
-        return true;
-    }
-
-    return false;
+    return taxAmountArr;
 }
 
-console.log(isPalindrome('nurses run'));
+taxAmountInCents(250, .18);
