aa28110c613ef6ff810fb38b4f1f39f09b1bf38d
xchanin <shannonwbruns@gmail.com>
2022-04-15 08:27:11 -0600

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,250 +1,80 @@
-(() => {
+/**
+ * Closures
+ */
 
-})();
+/**
+ * Example of closure
+ * 
+ * - Outer function 'count' is executed, creating an
+ *   instance of the inner function, the inner function 
+ *   exposes the variable counter, which is in its parent's
+ *   scope.
+ * 
+ * - We call the inner function from outside the scope in 
+ *   which it was declared. We did that by returning the 
+ *   inner function from the outer onr and storing a 
+ *   reference to it, named 'incrementCount,' to call it
+ * 
+ * - When the incrementCount function finishes running, normally
+ *   we would expect all of its variables to be garbage collected.
+ *   We'd experct each counter to go away when the function is done,
+ *   but they don't, because of a Closure.
+ * 
+ * - Since the inner function instances are still alive (assigned to incrementCount),
+ *   the closure is still preserving the counter variables.
+ * 
+ * @returns addCount function
+ */
+const count = () => {
 
-const integers = [1,2,3,4,5,6,7,8,9,10];
-const alphas = ['b','a','f','t','u'];
+    let counter = 0;
 
-const pilots = [
-    {
-      id: 10,
-      name: "Poe Dameron",
-      years: 14,
-    },
-    {
-      id: 2,
-      name: "Temmin 'Snap' Wexley",
-      years: 30,
-    },
-    {
-      id: 41,
-      name: "Tallissan Lintra",
-      years: 16,
-    },
-    {
-      id: 99,
-      name: "Ello Asty",
-      years: 22,
+    return () => {
+        counter += 1;
+        return counter;
     }
-  ];
+  
+}
 
-const objectToLoop = {
-one: {
-    id: 2,
-    name: "Wedge Antilles",
-    faction: "Rebels",
-    },
-    two: {
-    id: 8,
-    name: "Ciena Ree",
-    faction: "Empire",
-    },
-    three: {
-    id: 40,
-    name: "Iden Versio",
-    faction: "Empire",
-    },
-    four: {
-    id: 66,
-    name: "Thane Kyrell",
-    faction: "Rebels",
+/**
+ * Execute count function
+ */
+const runCounter = () => {
+
+    const incrementCount = count();
+
+    /**
+     * Quick loop, just so we can test this easier
+     */
+    for(let i = 0; i < 5; i++) {
+        console.log('INCREMENT COUNT ', incrementCount());
     }
 }
 
-const pilots2 = [
-{
-    id: 2,
-    name: "Wedge Antilles",
-    faction: "Rebels",
-},
-{
-    id: 8,
-    name: "Ciena Ree",
-    faction: "Empire",
-},
-{
-    id: 40,
-    name: "Iden Versio",
-    faction: "Empire",
-},
-{
-    id: 66,
-    name: "Thane Kyrell",
-    faction: "Rebels",
+runCounter();
+
+const countWithParams = (val) => {
+
+    if (!val) { return };
+
+    let counter = 0;
+
+    return () => {
+        counter += val;
+        return counter;
+    }
 }
-]
 
 /**
- * Slice - Returns elements in a new array. It selects the elements
- * starting at a given argument and ends at an option argument - if you 
- * omit the second argument then it selects till the end of the array
- * 
- * Slice won't mutate the original array
+ * Closure example with a parameter
  */
+const executeCountWithParams = () => {
 
-    /**
-     * Removes the first two items in the array and retrun them
-     * in a new array, then stop
-     */
-    console.log('SLICE ONE', [1,2,3,4,5,6,7,8,9,10].slice(0, 3));
-
-    /**
-     * Removes items from zero to the fourth position and leave everything else to the end
-     */
-    console.log('SLICE TWO', [1,2,3,4,5,6,7,8,9,10].slice(4));
-
-/**
- * Splice - insert, delete, replace
- */
-
-    /**
-     * Delete all but the first two items
-     * 
-     * Object.assign() - copy to new object, so we don't have the initial referrence
-     */
-    let remove = Object.assign([], integers);
-    remove.splice(0, 2);
-    console.log('Remove Items', remove); // returns [1,2]
-
-
-    /**
-     * Add items to an array
-     * First argument is the index where to place the item
-     * Seconod argument is zero, tells splice() to not delete anything
-     */            
-    let add = Object.assign([], integers);;
-    add.splice(3 , 0, 'a', 'b', 'c');
-    console.log('Add Items', add);
-
-    /**
-     * Replace an item in an array
-     */
-    let replace = Object.assign([], integers);
-    replace.splice(7,1, 'Replaced, sucka');
-    console.log('Replace Item', replace);
-
-/**
- * For-In used to loop over object
- * 
- * For-Of used to loop over arrays
- */
-
-    /**
-     * Loop over array of objects and m
-     */
-    const pilotsForEach = pilots;
-    pilotsForEach.forEach((item, index, arr) => {
-        item.id = item.id + 'changed-Item-id';
-    })
-
-    console.log(pilotsForEach);
-
-    /**
-     * For-In
-     */
-    for (const key in objectToLoop) {
-        const newId = objectToLoop[key].id + 'change-the-id';
-        objectToLoop[key].id = newId;
-        console.log('Looping over objects: ', key + ' : ' + objectToLoop[key].id);
+    const add = countWithParams(2);
+    
+    for(let i = 0; i < 5; i++) {
+        console.log('COUNT WITH PARAMS ', add(i));
     }
+}
 
-    /**
-     * For-Of
-     */
-    for (const item of pilots) {
-        console.log('Loop over arrays ', item.id);
-    }
-
-/**
- * Map, Reduce, Filter
- */
-    /**
-     * Map - create a new array of changed items from an array
-     */
-    const pilotsMap = pilots.map((item, index, arr) => {
-        return item.id + index + 'changed';
-    });
-
-    // console.log(pilotsMap);
-
-    /**
-     * Filter - return an new array of filtered items
-     */
-
-    const pilotsFilter = pilots2.filter((pilot) => {
-        return pilot.faction === 'Rebels';
-    });
-
-    // console.log(pilotsFilter);
-
-    /**
-     * Reduce - Executes a user supplied 'reducer' callback function of the array
-     * 
-     */
-
-    // easy example of reduce - return the sum of all flying years
-    const pilotTotalYearsOfService = pilots.reduce((acc, pilot) => {
-        return acc + pilot.years;
-    }, 0);
-
-    console.log(pilotTotalYearsOfService);
-
-   // find the pilot with the most years
-   const pilotWithMostYears = pilots.reduce((acc, pilot) => {
-       return (acc.years || 0) > pilot.years ? acc : pilot;
-   }, 0);
-
-   console.log(pilotWithMostYears);
-
-/**
-* Sort - sort an array (mutates)
-* default sorting works well for alphabetical, but not
-* great for numbers
-*/
-   /**
-    * Sort an array of alpha characters
-    */ 
-   const sortAlphaArray = Object.assign([], alphas);
-   sortAlphaArray.sort(); // sorts in ascending order
-
-   /**
-    * Reverse sortArray
-    */
-    sortAlphaArray.reverse(); // sort in descending order
-
-   console.log('SORT ALPHA ARRAY ', sortAlphaArray);
-
-   /**
-    * sort array of numbers - need a comparison function
-    */
-   const sortIntArray = Object.assign([], integers);
-   /**
-    * sort in ascending order
-    */
-   sortIntArray.sort((a,b) => {
-       return a - b;
-   });
-
-   /**
-    * sort in descending order
-    */
-   sortIntArray.sort((a,b) => {
-       return b - a;
-   });
-
-   console.log('SORT INT ARRAY ', sortIntArray);
-
-/**
-* Combine arrays
-*/
-   const combineArraySpread = [...integers, ...alphas];
-   console.log('Combine Object Assign ', combineArraySpread);
-
-   const combineArrayConcat = integers.concat(alphas);
-   console.log('Combine Array Concat ', combineArrayConcat);
-
-
-
-
-
-
+executeCountWithParams();
